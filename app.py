e2a7d07dc81b05504bb262a2cd15f6930e128381
serhernandez <sergio.hernandez@cgi.com>
2024-03-07 11:21:53 -0600

    Add basic functionality for chat entry and message storage

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def home_page():
+    if 'chat_history' not in session:
+        session['chat_history'] = []
+    return render_template('home.html', chat_history=session['chat_history'])
+
+@app.route("/", methods=['POST'])
+def process_data():
+    data = request.form.get('text')
+    response = f"I have processed {data}"
+    session['chat_history'].append(data)
+    session['chat_history'].append(response)
+    session.modified=True
+    print(session['chat_history'])
+    return response
