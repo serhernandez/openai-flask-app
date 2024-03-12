417071769a5a9bcd0020d34006a7edd41f24946f
serhernandez <sergio.hernandez@cgi.com>
2024-03-12 09:17:33 -0500

    Store settings in local db so they persist, and add list of other possible OpenAI models

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ API_KEY = os.getenv("OPENAI_API_KEY", "")
 assert API_KEY, "ERROR: OpenAI Key is missing"
 
 client = OpenAI(api_key=API_KEY)
-model = "gpt-3.5-turbo"
+openai_models = ["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"]
 #encoding = tiktoken.encoding_for_model(model)
 
 class Conversation(db.Model):
@@ -38,6 +38,11 @@ class FormattedMessage(db.Model):
     conversation_id = db.Column(db.Integer, db.ForeignKey(Conversation.id))
     conversation = db.relationship('Conversation', backref='formatted_messages')
 
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(30))
+    current = db.Column(db.Integer)
+
 @app.route("/")
 def home_page():
     if Conversation.query.count() == 0:
@@ -47,8 +52,14 @@ def home_page():
         db.session.add(sysMess)
         db.session.commit()
         session['current_conversation'] = 0
+    if Settings.query.count() == 0:
+        defaultSettings = Settings(model = openai_models[0], current=0)
+        db.session.add(defaultSettings)
+        db.session.commit()
     if 'current_conversation' not in session:
-        session['current_conversation'] = 0
+        session['current_conversation'] = Settings.query.with_entities(Settings.current).first()[0]
+    if 'current_model' not in session:
+        session['current_model'] = Settings.query.with_entities(Settings.model).first()[0]
     return render_template('home.html', chat_history=FormattedMessage.query.filter_by(conversation_id = session['current_conversation']).all(), chat_name = Conversation.query.filter_by(id = session['current_conversation']).with_entities(Conversation.name).first()[0], conversations = Conversation.query.all())
 
 @app.route("/", methods=['POST'])
@@ -60,7 +71,7 @@ def process_data():
     db.session.add(userContext)
     db.session.add(userFormatted)
     currentContext = list(map(lambda x: x[0], Context.query.with_entities(Context.content).filter_by(conversation_id=session['current_conversation']).all()))
-    completion = client.chat.completions.create(model=model, messages=currentContext, max_tokens=1000)
+    completion = client.chat.completions.create(model=session['current_model'], messages=currentContext, max_tokens=1000)
     response = completion.choices[0].message.content
     formatted_resp = markdown.markdown(response, extensions=['codehilite', 'fenced_code'])
     resp_dict = {"role": "assistant", "content": response}
@@ -86,6 +97,9 @@ def rename_chat():
 def select_chat():
     new_id = request.form.get('id')
     session['current_conversation'] = new_id
+    settings = Settings.query.first()
+    settings.current = new_id
+    db.session.commit()
     return '', 204
 
 @app.route("/newchat", methods=['POST'])
